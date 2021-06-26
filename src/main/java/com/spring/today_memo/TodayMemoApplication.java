452e2d22d1e70bd0d00c7f219a2a7f205f415be0
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-26 11:58:43 +0900

    Initial commit

---
+++ src/main/java/com/spring/today_memo/TodayMemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.today_memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayMemoApplication.class, args);
+    }
+
+}
